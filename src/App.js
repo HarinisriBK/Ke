87c0bf3f5f9d20751f877ba4ddb7a4e3bb68193c
harinisribk <harinisi495@gmail.com>
2024-01-25 16:00:15 +0530

    one

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Home from "./pages/Home"
+import Layout from "./Layout"
+import Contact from "./pages/Contact"
+import About from "./pages/About"
+import Service from "./pages/Service"
+export default function App(){
+  return(
+    <BrowserRouter>
+    <Routes>
+      <Route  path="/" element ={<Layout/>}>
+        <Route index element ={<Home/>}/>
+          <Route path="about" element={<About/>}/>
+          <Route path="contact" element={<Contact/>}/>
+          <Route path="service" element={<Service/>}/>
+      </Route>
+    </Routes>
+    </BrowserRouter>
+  )
+}
---
+++ src/pages/Service.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "../Service.css"
+export default function Footer() {
+  return (
+    <div>
+
+        <div class= 'grid-container'>
+            <div class = 'grid-item'>01</div>
+            <div class = 'grid-item'>02</div> 
+            <div class = 'grid-item'>03</div>
+            <div class = 'grid-item'>04</div>
+            <div class = 'grid-item'>05</div>
+            <div class = 'grid-item'>06</div>
+            
+        </div>
+    </div>
+  )
+}
